256cbac2b1b5b3daefd842ca70adbcbeeffbd403
Crystal Zhang <crystal7@ualberta.ca>
2022-01-15 13:50:57 -0700

    !!!

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 # python --> c
+dict_match={}
 
 
+def put_in_list(strings):
+    split = list(strings.split())
+
+
+
+
+if __name__ == "__main__":
+    put_in_list("hello world words balh")
+
+    pass
