7305805185eaa7111e9c429bd1f90900c4daa6c4
Crystal Zhang <crystal7@ualberta.ca>
2022-01-15 15:23:18 -0700

    !

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ def put_in_list(strings):
     split = list(strings.split())
     return split
 
+def range_split(var_in_brackets):
+    # split it again 
+
+    # return complete_split
+    pass
 
 def match_functions(split):
     for word in range(len(split)):
@@ -16,18 +21,26 @@ def match_functions(split):
                 split[word]=split[dict_match[key]]
     print(split)
 
+def for_loop_convert(split):
+    # taking in for var in range(start, end, step)
+    # take out brackets?
+    # Want: ['for', 'var', 'in', 'range',(start,', 'end,', 'step)']
+
+    #Make sure you can index every variable/word you want
+    #Match it to c++
+    # for i in split:
+    pass
+
 def output():
     print("#include <iostream>")
     print("using namespace std")
 
 if __name__ == "__main__":
-    x=put_in_list("math world words balh")
+    
+    x=put_in_list("for var in range(start, end, step)")
     y=match_functions(x)
-
+    extra_split = range_split(x)
+    z=for_loop_convert(x)
 
     pass
 
-def for_loop_convert():
-    python_loops = (list)
-    for i in python_loops:
-        
