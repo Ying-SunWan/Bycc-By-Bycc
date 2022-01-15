1b9f4c5e7320ac81593bd1b39afc4de4d78364c6
Crystal Zhang <crystal7@ualberta.ca>
2022-01-15 14:03:44 -0700

    !!!

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 # python --> c
 dict_match={}
 
-
 def put_in_list(strings):
     split = list(strings.split())
+    return split
 
 
+def output():
+    print("#include <iostream>")
+    print("using namespace std")
 
+
+def match_functions(split):
+    pass
 
 if __name__ == "__main__":
     put_in_list("hello world words balh")
