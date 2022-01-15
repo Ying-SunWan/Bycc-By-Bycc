902a176526d5866fed8cc3ccff4fa366c9330ab0
Crystal Zhang <crystal7@ualberta.ca>
2022-01-15 15:09:26 -0700

    !

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # python --> c
 dict_match={'math':'<cmath>',
-            'import':'<#include',
+            # 'import':'<#include',
+            #  time : '<ctime>', 
+            #  'sys' : '<iostream>', 
+            #  'random : '', os : '', urllib : '', re : '', cgi : '', socket : '',
+            #   string : '<string>'
 }
 
 def put_in_list(strings):
@@ -9,19 +13,21 @@ def put_in_list(strings):
 
 
 def match_functions(split):
+    new_string=[]
     for word in range(len(split)):
         for key in dict_match:
             if key == split[word]:
-                # new=split.replace(split[word],dict_match[key])
-                split[word]=split[dict_match[key]]
+                split[word]=split[word].replace(split[word],dict_match[key])
     print(split)
+    # issue: can't print out in correct format i.e. with replaced words and with words that aren't matched
 
 def output():
     print("#include <iostream>")
     print("using namespace std")
 
 if __name__ == "__main__":
-    x=put_in_list("math world words balh")
+    string = "math world words balh"
+    x=put_in_list(string)
     y=match_functions(x)
 
 
