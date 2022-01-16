9c01d79ce2179a521c73542b3f69c01bc11374fe
Ying Wan <90426228+Ying-SunWan@users.noreply.github.com>
2022-01-15 18:32:36 -0700

    updated to work with gui

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ dict_match={'math':'<cmath>',
 
 def put_in_list(strings):
     split = list(strings.split())
-    print("This is from put_in_list: {}".format(split))
+    #print("This is from put_in_list: {}".format(split))
     return split
 
 def bracket_split(no_brack_split):
@@ -15,7 +15,7 @@ def bracket_split(no_brack_split):
     for letter in no_brack_split:
         new_split+=letter.split("(")
 
-    print("This is from bracket_split: {} ".format (new_split))
+    #print("This is from bracket_split: {} ".format (new_split))
     return new_split
 
 def match_functions(split):
@@ -23,10 +23,10 @@ def match_functions(split):
         for key in dict_match:
             if key == split[word]:
                 split[word]=split[word].replace(split[word],dict_match[key])
-    print("This is from match: {}".format(split))
+    #print("This is from match: {}".format(split))
 
     return split
-
+'''
 def for_loop_convert(split_by_brackets):
     #Match it to c++
     # cpp_var = new_split[1]
@@ -35,14 +35,14 @@ def for_loop_convert(split_by_brackets):
     c_equiv = 'for ({0}, , {1}) {\n "write your code here"\n}'.format(initialize var, new_step)
     # for i in split:
     pass
-
+'''
 
 def input_change(inp):
     for i in range(len(inp)):
         if inp[i] == "input()":
             inp[i]=inp[i].replace(inp[i],"cin >>")
 
-    print("This is from input_change: {}".format(inp))
+    #print("This is from input_change: {}".format(inp))
     return inp
 
 def print_statements(print_state):
@@ -52,24 +52,22 @@ def print_statements(print_state):
         message = element[0:(len(element)-1)]
 
     out = "cout << {} <<endl;".format(message)
-    print("This is from print_statements: {}".format(out))
+    return out
+    #print("This is from print_statements: {}".format(out))
 
 
 def output():
     print("#include <iostream>")
     print("using namespace std;")
 
-if __name__ == "__main__":
-    test_string = "print(message)"
+def all(test_string):
+    test_string='print(message)'
     # TO DO: if there are functions i.e. def, don't run bracket_split
     
     split_into_string=put_in_list(test_string)
     split_by_brackets = bracket_split(split_into_string)
     match=match_functions(split_by_brackets)
     # loops=for_loop_convert(match)
-    input = input_change(match)
-    prints=print_statements(input)
-    
-
-    pass
-
+    inp = input_change(match)
+    prints=print_statements(inp)
+    return prints
