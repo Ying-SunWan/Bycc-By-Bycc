245f64304fe26674b1c79af7cf9c1342aac5bd2a
Crystal Zhang <crystal7@ualberta.ca>
2022-01-15 20:52:40 -0700

    !

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ dict_match={'math':'<cmath>',
 
 def put_in_list(strings):
     split = list(strings.split())
-    #print("This is from put_in_list: {}".format(split))
-    
+    print("This is from put_in_list: {}".format(split))
     return split
 
 def bracket_split(no_brack_split):
@@ -14,8 +13,8 @@ def bracket_split(no_brack_split):
     new_split = []
     for letter in no_brack_split:
         new_split+=letter.split("(")
-
-    #print("This is from bracket_split: {} ".format (new_split))
+    del new_split[6]
+    print("This is from bracket_split: {} ".format (new_split))
     return new_split
 
 def match_functions(split):
@@ -23,7 +22,7 @@ def match_functions(split):
         for key in dict_match:
             if key == split[word]:
                 split[word]=split[word].replace(split[word],dict_match[key])
-    #print("This is from match: {}".format(split))
+    print("This is from match: {}".format(split))
 
     return split
 
@@ -43,7 +42,7 @@ def input_change(inp):
         if inp[i] == "input()":
             inp[i]=inp[i].replace(inp[i],"cin >>")
 
-    # print("This is from input_change: {}".format(inp))
+    print("This is from input_change: {}".format(inp))
     return inp
 
 def print_statements(print_state):
@@ -52,12 +51,30 @@ def print_statements(print_state):
         # length_of_message = len(element)
         message = element[0:(len(element)-1)]
         out = "cout << {} <<endl;".format(message)
-        # print("This is from print_statements: {}".format(out))
+        print("This is from print_statements: {}".format(out))
     else:
         out = 0
         pass
     
     return out
+    
+def initialize_var(string_list):
+    string_list.insert(0, "int")
+    print("This is from initialize_var: {}".format(string_list))
+
+def function_convert(string_list):
+    string_list.append('{')
+    string_list[string_list.index('def')] = 'int'
+    print("This is from function_convert: {}".format(string_list))
+
+def return_and_curly(string_list):
+    string_list.append(';')
+    string_list.append('}')
+    #When printing?
+    print("This is from return_and_curly: {}".format(string_list))
+    ## Make sure to add a new line character between ; and } # when outputting
+    print(' '.join(map(str, string_list[0:3])))
+    print(string_list[3])
     
 
 def output():
@@ -65,13 +82,16 @@ def output():
     print("using namespace std;")
 
 if __name__ == "__main__" :
-    test_string='input(message)'
+    test_string='return var'
     # TO DO: if there are functions i.e. def, don't run bracket_split
     
     split_into_string=put_in_list(test_string)
-    split_by_brackets = bracket_split(split_into_string)
-    match=match_functions(split_by_brackets)
+    # split_by_brackets = bracket_split(split_into_string)
+    # match=match_functions(split_by_brackets)
     # loops=for_loop_convert(match)
-    inp = input_change(match)
-    prints=print_statements(inp)
+    # inp = input_change(match)
+    # prints=print_statements(inp)
+    # variables = initialize_var(split_into_string)
+    # functions=function_convert(split_into_string)
+    end_of_func=return_and_curly(split_into_string)
    
