53035858b5b6c77a6fa77f6ba9d8d94887d432c4
Ying Wan <90426228+Ying-SunWan@users.noreply.github.com>
2022-01-15 18:31:32 -0700

    updated to work with main

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from main import all
 # from https://www.youtube.com/watch?v=_XTy1-PYtPE
 
 # import translate_to_c from main
@@ -8,21 +9,15 @@ def translate_to_c(py_str):
     return c_str
 
 def show_answer():
-    inp_code.configure(state=tk.NORMAL)  # may not need
-    out.configure(state=tk.NORMAL)  # may not need
-
     out.delete(1.0, 'end-1c')  # clear out box
 
-    # new:
     inp = (inp_code.get(1.0, 'end-1c'))
     split_inp = inp.split('\n')
     output_code = ""
     for line in split_inp:
-        translated_line = translate_to_c(line)
+        translated_line = all(line)
         output_code += (translated_line + '\n')
-
-    out.insert(1.0, output_code)  # send output to out box
-    out.configure(state=tk.DISABLED)  # so when translated, the output cannot be edited by user
+        out.insert(1.0, output_code)  # send output to out box
 
 main = tk.Tk()
 # screen_w = main.winfo_screenwidth()
@@ -35,14 +30,11 @@ main = tk.Tk()
 tk.Label(main, text="Input Python:").grid(row=0, column=0, pady=6, sticky=tk.E+tk.W)
 tk.Label(main, text="Output C++:").grid(row=0, column=1, pady=6, sticky=tk.E+tk.W)
 
-
-
 # inp_code = tk.Text(main, width=int(main.winfo_screenwidth()/14))
 inp_code = tk.Text(main)
 out = tk.Text(main)
 inp_code.grid(row=1, column=0, pady=6, padx=6)
 out.grid(row=1, column=1, pady=6, padx=6)
-out.configure(state=tk.DISABLED)
 
 tk.Button(main, text='Close', command=main.destroy).grid(row=2, column=0, sticky=tk.W, pady=6)
 tk.Button(main, text='Translate', command=show_answer).grid(row=2, column=1, sticky=tk.W, pady=6)
